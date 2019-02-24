84e39b5f455f18ba9b9c6167562c43fb07a973aa
Austin Joyal <austinjk007@gmail.com>
2019-02-24 23:59:48 +0530

    Update wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -1,17 +1,17 @@
 from pyspark import SparkContext
 
-class RDD:
+class WordCount:
     def __init__(self, name):
         self.spark = SparkContext('local[3]', name)
 
-    def readRDD(self, path):
+    def read_RDD(self, path):
         return self.spark.textFile(path)
 
     def count_words(self, lines):
         return lines.filter(lambda x: len(x)).flatMap(lambda x: x.split()).count()
 
 if __name__ == '__main__':
-    rdd = RDD('wordcount')
-    lines = rdd.readRDD('/path/to/file/CountWords.txt')
+    rdd = WordCount('wordcount')
+    lines = rdd.read_RDD('/path/to/file/CountWords.txt')
     word_count = rdd.count_words(lines)
     print(word_count)
